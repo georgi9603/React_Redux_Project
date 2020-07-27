b36a8ac683bb28c3fbe0edc8dc3980d4cd3a8722
Георги <gtk960311@gmail.com>
2020-07-27 14:25:25 +0300

    Adding action and reducer with root reducer. Added comments to the code with notes from the course.

---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,22 @@
+export default function courseReducer(state = [], action) {
+    switch (action.type) {
+        case "CREATE_COURSE":
+            //state.push(action.course); It comes natural but it is WRONG ! This mutates state and state is immutable!
+            //We should return modified copy of state!
+            //Whatever is returned from reducer becomes new state for that particular reducer!
+            return [...state, { ...action.course }];
+        //Always return default state. If the reducer receivse action that it doesn't care about
+        //it should return the unchaged state
+        default:
+            return state
+    }
+}
+
+// For large data sets we can store by ID instead
+// E.G. const courses = {
+//     1:  {id: 1, title: "Course1"}
+//     2:  {id: 2, title: "Course2"}
+// }
+//In our data set we can use courses.find(course => course.id == 2) to find course with id 2
+//With the object data set we can  get the course by accessing the data set directly courses[2] which is performance benefitial
+//More info about the pattern in Normalizing State Shape in Redux documentation
