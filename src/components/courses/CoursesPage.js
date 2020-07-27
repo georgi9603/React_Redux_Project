4269dbfae0532dea9929aace848dfa3734b1aca6
Георги <gtk960311@gmail.com>
2020-07-27 14:04:35 +0300

    Adding on submit action to alert the input field in the form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -7,13 +7,21 @@ class CoursePage extends React.Component {
         }
     };
 
+    //Handling event change with arrow func remove the dependancy from binding this in constructor or in render form
     handleChange = event => {
+        //Copying the state instead of directly modifying it
         const course = { ...this.state.course, title: event.target.value };
         this.setState({ course });
     };
 
+    //Handling event change with arrow func remove the dependancy from binding this in constructor or in render form
+    handleSubmit = event => {
+        event.preventDefault();
+        alert(this.state.course.title);
+    }
+
     render() {
-        return <form>
+        return <form onSubmit={this.handleSubmit}>
             <h2>Courses</h2>
             <h3>
                 Add Course
