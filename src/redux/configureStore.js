cdd90d2c94a42a06d56f499ceaec164603e810ae
Георги <gtk960311@gmail.com>
2020-07-27 15:37:57 +0300

    Adding store configration

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import reduxImmutableStateInveriant from 'redux-immutable-state-invariant'
+
+export default function configureStore(initialState) {
+    const composeEnchancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose //add support for Redux dev tools
+
+    return createStore(
+        rootReducer,
+        initialState,
+        // reduxImmutableStateInveriant() "Dont'forget the brackets" => this will warn us if we accidentally mutate redux state
+        composeEnchancers(applyMiddleware(reduxImmutableStateInveriant()))
+    );
+}
