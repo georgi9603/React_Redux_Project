66578fc3e5fd7dcfa0cc30ad06743b85064a1cfa
Георги <gtk960311@gmail.com>
2020-08-06 22:33:46 +0300

    Fixing JSON error because of wrong url

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-    const composeEnhancers =
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose //add support for Redux dev tools
-    return createStore(
-        rootReducer,
-        initialState,
-        // reduxImmutableStateInveriant() "Dont'forget the brackets" => this will warn us if we accidentally mutate redux state
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-    );
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
 }
