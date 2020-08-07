4d379211314524433afa17f675c507625ae124da
Георги <gtk960311@gmail.com>
2020-08-07 18:18:35 +0300

    Adding routing and direct linking. Tested update.

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import CourseForm from './CourseForm';
 import { newCourse } from '../../../tools/mockData';
 
-function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, saveCourse, ...props }) {
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, saveCourse, history, ...props }) {
     const [course, setCourse] = useState({ ...props.course });
     const [errors, setErrors] = useState({});
 
@@ -15,6 +15,8 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, saveCour
             loadCourses().catch(error => {
                 alert('Loading courses failed ' + error)
             });
+        } else {
+            setCourse({ ...props.course })
         }
 
         if (authors.length === 0) {
@@ -22,7 +24,7 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, saveCour
                 alert('Loading authors failed ' + error)
             });
         }
-    }, [])
+    }, [props.course])
 
     function handleChange(event) {
         const { name, value } = event.target;
@@ -34,7 +36,9 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, saveCour
 
     function handleSave(event) {
         event.preventDefault();
-        saveCourse(course);
+        saveCourse(course).then(() => {
+            history.push("/courses")
+        });
     }
 
     return <CourseForm course={course} errors={errors} authors={authors} onChange={handleChange} onSave={handleSave} />
@@ -46,12 +50,19 @@ ManageCoursePage.propTypes = {
     courses: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
     loadAuthors: PropTypes.func.isRequired,
-    saveCourse: PropTypes.func.isRequired
+    saveCourse: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+    return courses.find(c => c.slug === slug) || null
+}
+
+function mapStateToProps(state, ownProps) {
+    const slug = ownProps.match.params.slug;
+    const course = slug && state.courses.length > 0 ? getCourseBySlug(state.courses, slug) : slug;
     return {
-        course: newCourse,
+        course,
         courses: state.courses,
         authors: state.authors
     };
